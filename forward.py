da0fa175a91375b4dbc979748960d97dff5e1868
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2023-12-12 10:52:13 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -173,7 +173,7 @@ async def forward(event):
                             continue
                         await asyncio.sleep(100)
 
-                legend.loop.run_until_complete(forward_message())
+                legend.loop.run_until_complete(forward_messages())
                 legend.run_until_disconnected()
         except Exception as e:
             await event.reply(f"Something Error : `{e}`")
