25e75dc3a0fb8f3edfd634326baca566deee9ef2
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2023-12-23 21:38:08 +0000

    pylint: auto fixes

--- forward.py
+++ forward.py
@@ -260,9 +260,7 @@ async def forward(event):
                     parts = message_link.text.split("/")
                     channel_username = parts[3]
                     message_id = int(parts[4])
-                    msg_id = await legend.get_messages(
-                        channel_username, ids=message_id
-                    )
+                    msg_id = await legend.get_messages(channel_username, ids=message_id)
                     while True:
                         for i in owo:
                             while cancelf == False:
